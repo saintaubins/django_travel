2c2fff875687a7b3c586e6e7b74ca6e465e92ed0
saintaubins <saintaubins@gmail.com>
2020-08-16 17:41:08 -0400

    saved more settings

--- travel/views.py
+++ travel/views.py
@@ -9,5 +9,5 @@ from django.http import HttpResponse
 def index(request):
     return render(request, 'index.html')
 
-def about(request):
-    return render(request, 'about.html')
+# def about(request):
+#     return render(request, 'about.html')
